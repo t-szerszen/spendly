66e283a6f8491c614470e7ee37d4717d4b8f4645
ronaldo7-tobi <t.szerszen@zset.leszno.pl>
2026-03-20 12:11:53 +0100

    Update: singleton bazy danych

---
+++ core/Database.php
@@ -0,0 +1,33 @@
+<?php
+class Database
+{
+	private static ?self $instance = null;
+
+	private PDO $pdo;
+
+	private function __construct()
+	{
+		$host = '';
+		$dbname = '';
+		$username = '';
+		$password = '';
+
+		try {
+			$this->pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4, $username, $password");
+			$this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+		} catch (PDOException $e) {
+			error_log('KRYTYCZNY BŁĄD POŁĄCZENIA Z BAZĄ: ' . $e->getMessage());
+			die('Wystąpił błąd serwera. Prosimy spróbować później.');
+		}
+	}
+	public static function getInstance(): self
+	{
+		if (self::$instance === null) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+}
