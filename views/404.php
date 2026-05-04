03f04538eb3bc5fdc4a9f1e8f21e97bb57587807
ronaldo7-tobi <t.szerszen@zset.leszno.pl>
2026-05-04 11:38:04 +0200

    Update: komponent head
    Stworzenie komponentu head, by ujednolicić sekcję head na stronach aplikacji.

--- views/404.php
+++ views/404.php
@@ -10,17 +10,8 @@ $title = $data['title'] ?? 'Błąd 404 - Strona nie istnieje';
 <!DOCTYPE html>
 <html lang="pl">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo htmlspecialchars($title); ?></title>
-    <!-- Google Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;800&display=swap" rel="stylesheet">
-    <!-- Styl CSS -->
-    <link rel="stylesheet" href="<?= url('styles/style.css') ?>?v=<?= time() ?>">
-</head>
+<!-- Head -->
+<?php include 'components/head.php'; ?>
 
 <body>
 
--- views/register.php
+++ views/register.php
@@ -10,16 +10,8 @@
 <!DOCTYPE html>
 <html lang="pl">
 
-<head>
-    <meta charset="UTF-8">
-    <title><?= $data['title'] ?></title>
-    <!-- Google Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;800&display=swap" rel="stylesheet">
-    <!-- Styl CSS -->
-    <link rel="stylesheet" href="<?= url('styles/style.css') ?>?v=<?= time() ?>">
-</head>
+<!-- Head -->
+<?php include 'components/head.php'; ?>
 
 <body>
     <?php include 'components/nav.php'; ?>
